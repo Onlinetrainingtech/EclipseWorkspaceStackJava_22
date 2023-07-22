9cad7bc774775f042305923432b3a7ae9d1a106f
MohamedAzarudeen <azartechnet@gmail.com>
2023-07-22 21:45:15 +0530

    22-07-2023

--- ProjectGUI/src/AddEmployee.java
+++ ProjectGUI/src/AddEmployee.java
@@ -4,9 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class AddEmployee extends JFrame {
 
@@ -75,19 +84,110 @@ public class AddEmployee extends JFrame {
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
-		JButton btnNewButton = new JButton("AddProduct");
+		final JButton btnNewButton = new JButton("AddEmployee");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try
+				{
+					//int empid=Integer.parseInt(textField.getText());
+					String empid=textField.getText();
+					String empname=textField_1.getText();
+					String salary=textField_2.getText();
+					String str1="insert into addemployee values('"+empid+"','"+empname+"','"+salary+"')";
+					Class.forName("org.h2.Driver");
+					Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/javap22","sa","");
+					Statement stmt=conn.createStatement();
+					stmt.executeUpdate(str1);
+					JOptionPane.showMessageDialog(btnNewButton,"Inserted...");
+				}
+				catch(ClassNotFoundException t)
+				{
+					
+				}
+				catch(SQLException t1)
+				{
+					
+				}
+			}
+		});
 		btnNewButton.setBounds(10, 210, 85, 21);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Search");
+		final JButton btnNewButton_1 = new JButton("Search");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				try
+				{
+					String empid=textField.getText();
+					String str2="select * from addemployee where empid='"+empid+"'";
+					Class.forName("org.h2.Driver");
+					Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/javap22","sa","");
+					Statement stmt=conn.createStatement();
+					ResultSet rs=stmt.executeQuery(str2);
+					rs.next();
+					String t1=rs.getString(1);
+					String t2=rs.getString(2);
+					textField_1.setText(t1);
+					textField_2.setText(t2);
+					
+					JOptionPane.showMessageDialog(btnNewButton_1,"Search!!!");
+					
+				}
+				catch(Exception t)
+				{
+					System.out.println(t);
+				}
+			}
+		});
 		btnNewButton_1.setBounds(147, 210, 85, 21);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Update");
+		final JButton btnNewButton_2 = new JButton("Update");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				try
+				{
+					String empid=textField.getText();
+					String empname=textField_1.getText();
+					String str3="update addemployee set empname='"+empname+"' where empid='"+empid+"'";
+					Class.forName("org.h2.Driver");
+					Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/javap22","sa","");
+					Statement stmt=conn.createStatement();
+					stmt.executeUpdate(str3);
+					JOptionPane.showMessageDialog(btnNewButton_2,"Updated...");
+				}
+				catch(Exception t)
+				{
+					System.out.println(t);
+				}
+			}
+		});
 		btnNewButton_2.setBounds(270, 210, 85, 21);
 		contentPane.add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("Delete");
+		final JButton btnNewButton_3 = new JButton("Delete");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				try
+				{
+					String empid=textField.getText();
+					String str4="delete addemployee where empid='"+empid+"'";
+					Class.forName("org.h2.Driver");
+					Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/javap22","sa","");
+					Statement stmt=conn.createStatement();
+					stmt.executeUpdate(str4);
+					JOptionPane.showMessageDialog(btnNewButton_3,"Deleted...");
+				}
+				catch(Exception t)
+				{
+					
+				}
+			}
+		});
 		btnNewButton_3.setBounds(321, 150, 85, 21);
 		contentPane.add(btnNewButton_3);
 	}
